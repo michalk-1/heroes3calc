2a181b30778b7571d469f605f4a60f0af5beb7b8
Michal Kowalik <mich.kowalik@gmail.com>
2018-11-20 05:58:21 +0100

    Refactor Result component.

--- src/components/AttackResult/index.js
+++ src/components/AttackResult/index.js
@@ -1,32 +1,45 @@
 import React from 'react';
 import style from './AttackResult.css';
 
+export function formatResult(props) {
+    const losses = props.minimum_losses === props.maximum_losses ?
+      <span>{props.minimum_losses}</span> : (
+      <span>
+        {props.minimum_losses} {' '}-{' '} {props.maximum_losses}
+        {' '}(avg {props.average_losses})
+      </span>
+    );
+    const damage = props.minimum_damage === props.maximum_damage ?
+      <span>{props.minimum_damage}</span> : (
+      <span>
+        {props.minimum_damage} {' '}-{' '} {props.maximum_damage}
+        {' '}(avg {props.average_damage})
+      </span>
+    );
+    const remaining = (
+      props.minimum_units_left === props.maximum_units_left ?
+      <span>{props.minimum_units_left}</span> : (
+      <span>
+        {props.minimum_units_left} {' '}-{' '}
+        {props.maximum_units_left} {' '}
+        (avg {props.average_units_left})
+      </span>
+    ));
+    return {
+      losses: losses,
+      damage: damage,
+      remaining: remaining,
+    }
+}
 
 export class AttackResult extends React.Component {
   render() {
+    const result = formatResult(this.props);
     return (
       <div className={style['attack-result']}>
-        <p>
-          Damage:{' '}
-          <span>{this.props.minimum_damage}</span>
-          {' '}-{' '}
-          <span>{this.props.maximum_damage}</span>
-          {' '}(<span>avg {this.props.average_damage}</span>)
-        </p>
-        <p>
-          Losses:{' '}
-          <span>{this.props.minimum_losses}</span>
-          {' '}-{' '}
-          <span>{this.props.maximum_losses}</span>
-          {' '}(<span>avg {this.props.average_losses}</span>)
-        </p>
-        <p>
-          Remaining:{' '}
-          <span>{this.props.minimum_units_left}</span>
-          {' '}-{' '}
-          <span>{this.props.maximum_units_left}</span>
-          {' '}(<span>avg {this.props.average_units_left}</span>)
-        </p>
+        <p>Damage:{' '}{result.damage}</p>
+        <p>Losses:{' '}{result.losses}</p>
+        <p>Remaining:{' '}{result.remaining}</p>
       </div>
     );
   }
--- src/components/RetaliationResult/index.js
+++ src/components/RetaliationResult/index.js
@@ -1,33 +1,15 @@
 import React from 'react';
 import style from './RetaliationResult.css';
-import { calcAverage } from '../../calc-lib.js';
-
+import { formatResult } from '../AttackResult/index.js';
 
 export class RetaliationResult extends React.Component {
   render() {
+    const result = formatResult(this.props);
     return (
       <div className={style['retaliation-result']}>
-        <p>
-          Losses:{' '}
-          <span>{this.props.minimum_losses}</span>
-          {' '}-{' '}
-          <span>{this.props.maximum_losses}</span>
-          {' '}(<span>avg {this.props.average_losses}</span>)
-        </p>
-        <p>
-          Damage:{' '}
-          <span>{this.props.minimum_damage}</span>
-          {' '}-{' '}
-          <span>{this.props.maximum_damage}</span>
-          {' '}(<span>avg {this.props.average_damage}</span>)
-        </p>
-        <p>
-          Remaining:{' '}
-          <span>{this.props.minimum_units_left}</span>
-          {' '}-{' '}
-          <span>{this.props.maximum_units_left}</span>
-          {' '}(<span>avg {this.props.average_units_left}</span>)
-        </p>
+        <p>Losses:{' '}{result.losses}</p>
+        <p>Damage:{' '}{result.damage}</p>
+        <p>Remaining:{' '}{result.remaining}</p>
       </div>
     );
   }
