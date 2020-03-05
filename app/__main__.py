7e4c042f25111b242a4f092e1d2cc92971101034
Michal Kowalik <mich.kowalik@gmail.com>
2020-03-05 20:50:36 +0100

    add data

--- app/__main__.py
+++ app/__main__.py
@@ -1,4 +1,5 @@
 import os
+from functools import partial
 from pathlib import Path
 
 from flask import (
@@ -14,27 +15,116 @@ from flask import (
 from app.via import csv as via_csv
 
 
+def one_stack(name, number):
+    return {"name": name, "number": number}
+
+
+def five_stacks(name, number):
+    assert number % 5 == 0, number
+    return [one_stack(name, number // 5)] * 5
+
+
+skeleton = partial(one_stack, "skeleton")
+walking_dead = partial(one_stack, "walking_dead")
+wight = partial(one_stack, "wight")
+vampire = partial(one_stack, "vampire")
+green_dragon = partial(one_stack, "green_dragon")
+red_dragon = partial(one_stack, "red_dragon")
+gold_dragon = partial(one_stack, "gold_dragon")
+black_dragon = partial(one_stack, "black_dragon")
+
+
+# TODO: expand to royal griffins etc; disregard probabilities?
 CREATURE_BANKS = {
-    "black_tower": {},
-    "crypt": {},
-    "dragon_fly_hive": {},
-    "dragon_utopia": {},
-    "dwarven_treasury": {},
-    "experimental_shop": {},
-    "griffin_conservatory": {},
-    "imp_cache": {},
-    "mansion": {},
-    "medusa_stores": {},
-    "naga_bank": {},
-    "red_tower": {
-        "levels": [
-            {"guards": [{"name": "green_dragon", "number": 1}]},
-            {"guards": [{"name": "red_dragon", "number": 1}]},
-            {"guards": [{"name": "gold_dragon", "number": 1}]},
-            {"guards": [{"name": "black_dragon", "number": 1}]},
-        ]
-    },
-    "wolf_raider_picket": {},
+    "black_tower": [
+        {"guards": [green_dragon(1)]},
+        {"guards": [red_dragon(1)]},
+        {"guards": [gold_dragon(1)]},
+        {"guards": [black_dragon(1)]},
+    ],
+    "crypt": [
+        {
+            "guards": [
+                skeleton(10),
+                walking_dead(10),
+                walking_dead(10),
+                skeleton(10),
+                skeleton(10),
+            ]
+        },
+        {
+            "guards": [
+                skeleton(13),
+                walking_dead(10),
+                wight(5),
+                walking_dead(10),
+                skeleton(12),
+            ]
+        },
+        {"guards": [skeleton(20), walking_dead(20), wight(10), vampire(5)]},
+        {"guards": [skeleton(20), walking_dead(20), wight(10), vampire(10)]},
+    ],
+    "dragon_fly_hive": [
+        {"guards": five_stacks("dragon_fly", 30)},
+        {"guards": five_stacks("dragon_fly", 45)},
+        {"guards": five_stacks("dragon_fly", 60)},
+        {"guards": five_stacks("dragon_fly", 90)},
+    ],
+    "dragon_utopia": [
+        {"guards": [green_dragon(8), red_dragon(5), gold_dragon(2), black_dragon(1)]},
+        {"guards": [green_dragon(8), red_dragon(6), gold_dragon(3), black_dragon(2)]},
+        {"guards": [green_dragon(8), red_dragon(6), gold_dragon(4), black_dragon(3)]},
+        {"guards": [green_dragon(8), red_dragon(7), gold_dragon(6), black_dragon(5)]},
+    ],
+    "dwarven_treasury": [
+        {"guards": five_stacks("dwarf", 50)},
+        {"guards": five_stacks("dwarf", 75)},
+        {"guards": five_stacks("dwarf", 100)},
+        {"guards": five_stacks("dwarf", 150)},
+    ],
+    "experimental_shop": [
+        {"guards": five_stacks("steel_golem", 25)},
+        {"guards": five_stacks("steel_golem", 50)},
+        {"guards": five_stacks("steel_golem", 75)},
+        {"guards": five_stacks("steel_golem", 100)},
+    ],
+    "griffin_conservatory": [
+        {"guards": five_stacks("griffin", 50)},
+        {"guards": five_stacks("griffin", 100)},
+        {"guards": five_stacks("griffin", 150)},
+        {"guards": five_stacks("griffin", 200)},
+    ],
+    "imp_cache": [{"guards": []}],
+    "mansion": [
+        {"guards": five_stacks("vampire_lord", 40)},
+        {"guards": five_stacks("vampire_lord", 60)},
+        {"guards": five_stacks("vampire_lord", 80)},
+        {"guards": five_stacks("vampire_lord", 100)},
+    ],
+    "medusa_stores": [
+        {"guards": five_stacks("medusa", 20)},
+        {"guards": five_stacks("medusa", 30)},
+        {"guards": five_stacks("medusa", 40)},
+        {"guards": five_stacks("medusa", 50)},
+    ],
+    "naga_bank": [
+        {"guards": five_stacks("naga", 10)},
+        {"guards": five_stacks("naga", 15)},
+        {"guards": five_stacks("naga", 20)},
+        {"guards": five_stacks("naga", 30)},
+    ],
+    "red_tower": [
+        {"guards": five_stacks("fire_elemental", 35)},
+        {"guards": five_stacks("fire_elemental", 70)},
+        {"guards": five_stacks("fire_elemental", 105)},
+        {"guards": five_stacks("fire_elemental", 140)},
+    ],
+    "wolf_raider_picket": [
+        {"guards": five_stacks("wolf_raider", 50)},
+        {"guards": five_stacks("wolf_raider", 75)},
+        {"guards": five_stacks("wolf_raider", 100)},
+        {"guards": five_stacks("wolf_raider", 150)},
+    ],
 }
 
 
@@ -93,10 +183,8 @@ def create_app(test_config=None):
             o = {
                 "name": name,
                 "image": str(file_path.relative_to(app.root_path)),
+                "creature_bank": CREATURE_BANKS[name],
             }
-            g = CREATURE_BANKS[name]
-            print(g)
-            o.update(g)
             xs.append(o)
 
         return jsonify({"banks": xs})
