03c432005c8c7500ed6aefcd5a3a2b038c837354
Michal Kowalik <mich.kowalik@gmail.com>
2020-03-22 20:20:46 +0100

    Add Todos.

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Calc } from './components/Calc/index.js';
-import { CreatureBank } from './components/CreatureBank/index.js';
-import { asyncGetBank } from './components/Creatures/index.js';
 
-export class CreatureBankApp extends React.Component {
+/*
+Todo:
+- Optimize additional attack and number to one-hit on average.
+- Disable editing in inputs that are pre-populated.
+- Rearrange the layout of top level components to more tight and aligned.
+*/
+
+export class DebugApp extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
-    asyncGetBank('Griffin Conservatory').then(bank => { this.setState({bank: bank}); });
-    asyncGetBank('Dwarven Treasury').then(bank => { this.setState({bank2: bank}); });
   }
 
   render() {
-    const props = this.props;
-    const bank = this.state.bank;
-    const bank2 = this.state.bank2;
     return (
-      <div className="sth">
-        <CreatureBank bank={bank} onGuardClick={guard => console.log(guard.toJS())} />
-        <CreatureBank bank={bank2} onGuardClick={guard => console.log(guard.toJS())} />
+      <div>
+        {Object.keys(this.props).map(key => <div>{this.props[key]}</div>)}
+        {Object.keys(this.state).map(key => <div>{this.state[key]}</div>)}
       </div>
     );
   }
