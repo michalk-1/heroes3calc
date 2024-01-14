76504479b210a84938feaa9bbe3b1a884884e816
michal <mich.kowalik@gmail.com>
2024-01-14 21:48:11 +0100

    Refactor a function.

--- app/filelib.py
+++ app/filelib.py
@@ -1,9 +1,8 @@
 from pathlib import Path
 
 
-def make_directory(directory: Path):
-    if not directory.is_dir():
-        if directory.exists():
-            raise RuntimeError(f'Path {directory} exists, but not as a directory.')
+def make_directory(directory: Path) -> None:
+    if not directory.is_dir() and directory.exists():
+        raise RuntimeError(f"Path {directory} exists, but is not as a directory.")
 
-        directory.mkdir(parents=True)
+    directory.mkdir(parents=True)
